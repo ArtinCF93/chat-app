7968d393f99890367113cfc2abd0a32f87bf09e7
ArtinCF93 <Toojool@gmail.com>
2022-01-06 19:35:59 -0800

    Update chat.js

--- components/chat.js
+++ components/chat.js
@@ -16,30 +16,22 @@ class Chat extends React.Component {
             uid: 0
         }
 
-        if (!firebase.apps.length) {
-            firebase.initializeApp({
+        if (!firebase.default.apps.length) {
+            firebase.default.initializeApp({
                 apiKey: "AIzaSyAHk6btELvaiNCSjeuEUCxJUQaBCPoTpSs",
                 authDomain: "chatapp-f8893.firebaseapp.com",
                 projectId: "chatapp-f8893",
                 storageBucket: "chatapp-f8893.appspot.com",
                 messagingSenderId: "952846402386",
                 appId: "1:952846402386:web:2075f4aad2329b17f2f08b"
-              });
+            });
         }
-        //this references a specific collection from firebase
-        this.referenceChatMessages = firebase.firestore().collection('messages');
+        this.referenceChatMessageUser = null;
     };
 
 
     onSend(messages = []) { //the paramter is the object array from the state
-        //paramter 'previousState' is a reference to the component's state at the time the change is applied
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-            //this appends the new message to the messages object with append()
-            //the first parameter in append() is the old state, while second is the new.
-        }),
-        () => {this.addMessage();}
-        )
+        this.addMessage(messages[0]);
     }
 
 
@@ -62,15 +54,13 @@ class Chat extends React.Component {
 
     //this function retrieves the curent data in your collection and store it in my 'lists' state
     onCollectionUpdate = (querySnapshot) => {
-        let name = this.props.route.params.name;
         let messages = [];
         //go through each document; querySnapshot is all the data currently in my collection
         querySnapshot.forEach((doc) => {
             let data = doc.data();
             messages.push({ //each field is saved into the lists object
-                uid: data._id,
-                text: data.text,
-                name: name,
+                ...data,
+                createdAt: data.createdAt.toDate(),
             });
         });
         this.setState({ //the fields are saved and rendered by the setState() function
@@ -78,48 +68,41 @@ class Chat extends React.Component {
         })
     }
 
-    addMessage() {
-        let name = this.props.route.params.name;
-        this.referenceChatMessages.add({
-            uid: this.state.uid,
-            name: name,
-            text: 'test message'
-        })
+    addMessage(message) {
+        this.referenceChatMessages.add(
+            message
+        )
     }
 
 
     componentDidMount() {
         let name = this.props.route.params.name;
-        this.referenceChatMessages = firebase.firestore().collection('messages');
+
+        //this references a specific collection from firebase
+        this.referenceChatMessages = firebase.default.firestore().collection('messages');
 
         //authorizing specific user
         //firebase.auth().onAuthStateChanged is what calls the firebase auth service
-        this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+        this.authUnsubscribe = firebase.default.auth().onAuthStateChanged(async (user) => {
             if (!user) {
-                await firebase.auth().signInAnonymously();
+                await firebase.default.auth().signInAnonymously();
             }
             this.setState({
-                uid: user.uid,
                 loggedInText: 'Hello there ' + name,
-                user: {
-                    _id: user.uid,
-                    name: name,
-                    avatar: 'https://placeimg.com/140/140/any',
-                },
+                uid: user.uid
             });
-            //this references the specific user messages by uid within specific collection 
-            // this.referenceChatMessageUser = firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
-            // this.unsubscribeChatMessageUser = this.referenceChatMessageUser.onSnapshot(this.onCollectionUpdate);
+            //this references the specific user messages by uid within specific collection
+            //the function below matches the message with the uid in the user object, to only show messag
+            this.referenceChatMessageUser = firebase.default.firestore().collection('messages').where("user", "==", this.state.uid);
+            this.unsubscribeChatMessageUser = this.referenceChatMessageUser.onSnapshot(this.onCollectionUpdate);
             //snapshot function returns how the data looks in the present time
         })
-        this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate);
     }
 
 
     componentWillUnmount() {
-        // this.authUnsubscribe();
-        // this.unsubscribeChatMessageUser();
-        this.unsubscribe();
+        this.authUnsubscribe();
+        this.unsubscribeChatMessageUser();
     }
 
 
@@ -137,9 +120,10 @@ class Chat extends React.Component {
                     renderBubble={this.renderBubble.bind(this)}
                     messages={this.state.messages}
                     onSend={messages => this.onSend(messages)}
-                    user={{
-                        _id: 2
-                    }}
+                    user={
+                        this.state.uid //this saves user as an object in database {}
+                        // storing the uid in the user object in giftedchat to be refferred to in using the referenceChatMessageUser function in component did mount
+                    }
                 />
                 {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
             </View>
