1e23070881f8755a175e72ca3a80f998a7fa132e
ArtinCF93 <Toojool@gmail.com>
2022-01-09 21:37:01 -0800

    Update chat.js

--- components/chat.js
+++ components/chat.js
@@ -11,12 +11,17 @@ class Chat extends React.Component {
     constructor() {
         super();
         this.state = {
-            messages: [],
             loggedInText: 'Please wait until you are logged in',
-            uid: 0
+            messages: [],
+            uid: 0,
+            user: {
+                _id: "",
+                name: "",
+                avatar: "",
+              }
         }
 
-        if (!firebase.default.apps.length) {
+        if (!firebase.default.apps.length) { //firebase integration
             firebase.default.initializeApp({
                 apiKey: "AIzaSyAHk6btELvaiNCSjeuEUCxJUQaBCPoTpSs",
                 authDomain: "chatapp-f8893.firebaseapp.com",
@@ -30,10 +35,13 @@ class Chat extends React.Component {
     };
 
 
-    onSend(messages = []) { //the paramter is the object array from the state
-        this.addMessage(messages[0]);
-    }
-
+    onSend(messages = []) {
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }), () => {
+          this.addMessages();
+        })
+      }
 
     renderBubble(props) {
         return (
@@ -54,24 +62,36 @@ class Chat extends React.Component {
 
     //this function retrieves the curent data in your collection and store it in my 'lists' state
     onCollectionUpdate = (querySnapshot) => {
-        let messages = [];
+        const messages = [];
         //go through each document; querySnapshot is all the data currently in my collection
         querySnapshot.forEach((doc) => {
             let data = doc.data();
-            messages.push({ //each field is saved into the lists object
-                ...data,
+            messages.push({
+                uid: data.uid,
+                text: data.text,
                 createdAt: data.createdAt.toDate(),
+                user: {
+                    _id: data.user._id,
+                    name: data.user.name,
+                    avatar: data.user.avatar
+                  },
             });
         });
-        this.setState({ //the fields are saved and rendered by the setState() function
-            messages
-        })
-    }
+        this.setState({
+            messages: messages
+        });
+    };
 
-    addMessage(message) {
-        this.referenceChatMessages.add(
-            message
-        )
+
+    addMessages() {
+        let message = this.state.messages[0];
+        // add a new messages to the collection
+        this.referenceChatMessages.add({
+            _id: message._id,
+            text: message.text,
+            createdAt: message.createdAt,
+            user: this.state.user
+        });
     }
 
 
@@ -79,35 +99,45 @@ class Chat extends React.Component {
         let name = this.props.route.params.name;
 
         //this references a specific collection from firebase
-        this.referenceChatMessages = firebase.default.firestore().collection('messages');
+        this.referenceChatMessages = firebase.default.firestore().collection("messages");
+        // listens for updates in the collection
+        this.unsubscribe = this.referenceChatMessages.orderBy("createdAt", "desc").onSnapshot(this.onCollectionUpdate)
 
         //authorizing specific user
         //firebase.auth().onAuthStateChanged is what calls the firebase auth service
         this.authUnsubscribe = firebase.default.auth().onAuthStateChanged(async (user) => {
             if (!user) {
-                await firebase.default.auth().signInAnonymously();
+                return await firebase.default.auth().signInAnonymously();
             }
+            //update user state with currently active user data
             this.setState({
+                uid: user.uid,
                 loggedInText: 'Hello there ' + name,
-                uid: user.uid
+                messages: [],
+                user: {
+                    _id: user.uid,
+                    name: name,
+                    avatar: "https://placeimg.com/140/140/any",
+                  },
             });
             //this references the specific user messages by uid within specific collection
             //the function below matches the message with the uid in the user object, to only show messag
-            this.referenceChatMessageUser = firebase.default.firestore().collection('messages').where("user", "==", this.state.uid);
-            this.unsubscribeChatMessageUser = this.referenceChatMessageUser.onSnapshot(this.onCollectionUpdate);
+            this.unsubscribeChatMessageUser = firebase.default.firestore().collection("messages").where("uid", "==", this.state.uid);
             //snapshot function returns how the data looks in the present time
-        })
+        });
     }
 
 
     componentWillUnmount() {
         this.authUnsubscribe();
         this.unsubscribeChatMessageUser();
+        this.unsubscribe();
     }
 
 
     render() {
         let backgroundColor = this.props.route.params.backgroundColor;
+        let name = this.props.route.params.name;
 
         return (
             <View style={[styles.container, { backgroundColor: backgroundColor }]}>
@@ -120,10 +150,12 @@ class Chat extends React.Component {
                     renderBubble={this.renderBubble.bind(this)}
                     messages={this.state.messages}
                     onSend={messages => this.onSend(messages)}
-                    user={
-                        this.state.uid //this saves user as an object in database {}
+                    user={{
+                        _id: this.state.user._id, //this saves user as an object in database {}
                         // storing the uid in the user object in giftedchat to be refferred to in using the referenceChatMessageUser function in component did mount
-                    }
+                        name: name,
+                        avatar: this.state.user.avatar,
+                    }}
                 />
                 {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
             </View>
